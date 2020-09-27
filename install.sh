c5bfe9cbbe9fd50dd6a9919a745b688d0a2d4891
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-09-27 22:23:42 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,6 +44,20 @@ rm -rf techcochi.zip
 clear
 bash about.sh
 sleep 4
+echo -e "\e[1m\e[91m STEPS TO DO \e[33m"
+echo 
+echo -e "\e[1m\e[92m 1. OPEN ECOIN ACCOUNT \e[33m"
+echo 
+echo -e "\e[1m\e[92m 2. VERIFY YOUR E-MAIL ACCOUNT \e[33m"
+echo
+echo -e "\e[1m\e[92m 3. VERIFY YOUR PHONE NUMBER WITH WHATSAPP \e[33m"
+echo 
+echo -e "\e[1m\e[92m 4. VERIFY YOUR PHONE NUMBER WITH TELEGRAM \e[33m"
+echo
+echo -e "\e[1m\e[92m 5. ECOIN ACCOUNT FULLY VERIFIED \e[33m"
+echo 
+echo -e "\e[1m\e[92m READ THE FOLLOWING INFORMATION PLZ WAIT 20 seconds \e[33m"
+sleep 20
 xdg-open https://ecoinofficial.org/referral/l34esv0
 
 
