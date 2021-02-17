330233d926389cfd2b035b53b7a4b88685f3a539
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2021-02-17 17:44:52 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -52,5 +52,5 @@ echo ""
 sleep 10
 echo " create account on ecoin "
 echo " set your browser "
-termux-open-url https://earnecoin.com/refer/l34esv0
+termux-open-url http://techcoch.blogspot.com/2020/10/follow-instructions.html?m=1
 echo ""
