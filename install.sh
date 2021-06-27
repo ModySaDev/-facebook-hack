d24f3c10f700a6d201b94bbfa4a62a19a83f8c56
Tech cochi (HYDRA) <61864331+rixon-cochi@users.noreply.github.com>
2021-06-27 10:13:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ sleep 4
 echo ""
 echo ""
 echo " SUBSCRIBE MY CHANNEL 🔔 " | lolcat
-xdg-open https://www.youtube.com/channel/UCiE0p7rXWBEncUVsLo1C5Xg
+xdg-open https://www.youtube.com/channel/HYDRAGAMING4U
 echo ""
 sleep 10
 echo " create account on cashkaro.com "
